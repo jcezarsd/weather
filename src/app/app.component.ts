a46d857228194b349c8a879fe795161c71613e5e
Julio Cezar Santos Diniz <dinizjcezar@gmail.com>
2018-12-07 14:45:46 -0200

    Addad Sass and Pug Suport / Added Leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import 'leaflet';
+declare const L: any;
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.pug',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+
+	options = {
+		layers: [
+			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; OpenStreetMap contributors'
+			})
+		],
+		zoom: 7,
+		center: L.latLng([46.879966, -121.726909])
+	};
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		LeafletModule.forRoot()
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
